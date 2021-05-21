b4e01a95f39d55f681ab16e876f429bd0b8d03c6
Reece Dunham <me@rdil.rocks>
2021-05-21 10:50:19 -0400

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head"
+import React from "react"
+import Header from "../src/Header"
+
+export default function Custom404() {
+    return (
+        <div className="container">
+            <Head>
+                <title>Notex.app - Page not found</title>
+            </Head>
+
+            <Header />
+
+            <main className="main">
+                <h1 className="title">404</h1>
+                <h2 className="description">Page not found.</h2>
+            </main>
+        </div>
+    )
+}
