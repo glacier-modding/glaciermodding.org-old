d1a3f2ee5a1cec5551eb979659452f678005993b
Reece Dunham <me@rdil.rocks>
2021-08-09 13:26:34 -0400

    Pre-render material ui components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "../styles/globals.css"
+import * as React from "react"
 
 function App({ Component, pageProps }) {
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector("#jss-server-side")
+        if (jssStyles) {
+            jssStyles.parentElement!.removeChild(jssStyles)
+        }
+    }, [])
+
     return <Component {...pageProps} />
 }
 
