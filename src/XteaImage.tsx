95322c61fa3003a141dd5eda1e6f30eba0a4d356
hitman-resources <24306974+hitman-resources@users.noreply.github.com>
2021-08-16 16:40:52 +1000

    Update XteaImage.tsx

--- src/XteaImage.tsx
+++ src/XteaImage.tsx
@@ -1,7 +1,7 @@
 export default function XteaImage() {
     return (
         <div id="xtealogo">
-            <img className="xtealogo" src="/xtea.png" alt="XTEA logo." />
+            <img className="xtealogo" src="/xtea.png" alt="XTEA logo." style="max-width:200px" />
         </div>
     )
 }
