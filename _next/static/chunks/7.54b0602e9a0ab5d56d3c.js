12a2bc8b804854a1ba1c2ee8c5f95de905c79092
RDIL <RDIL@users.noreply.github.com>
2021-08-16 18:42:50 +0000

    deploy: 21fd730bef4c4f83e26f4f1b2eb2bb8e36244975

---
+++ _next/static/chunks/7.54b0602e9a0ab5d56d3c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7],{3007:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});var a=t(2010);function s(){return(0,a.jsx)("div",{id:"xtealogo",children:(0,a.jsx)("img",{className:"xtealogo",src:"/xtea.png",alt:"XTEA logo."})})}}}]);
