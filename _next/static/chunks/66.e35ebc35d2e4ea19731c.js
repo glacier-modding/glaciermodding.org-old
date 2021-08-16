3390177ae253d06cfdce4ddf9e70baae75ac63cd
Notexe <Notexe@users.noreply.github.com>
2021-08-16 13:05:37 +0000

    deploy: f6cc8e6335a9ac791132c287d7a86b4a84a7acff

---
+++ _next/static/chunks/66.e35ebc35d2e4ea19731c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[66],{66:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return s}});var a=t(2010);function s(){return(0,a.jsx)("div",{id:"xtealogo",children:(0,a.jsx)("img",{className:"xtealogo",src:"/xtea.png",alt:"XTEA logo."})})}}}]);
